20e5b79e2dba9291e27104ade1f8478aff974188
ahmadShahzad <128154734+ahmad9038@users.noreply.github.com>
2023-11-23 09:42:58 +0500

    Update app.js

--- app.js
+++ app.js
@@ -26,13 +26,13 @@ app.use(fetchDataRouter);
 app.use(chatRouter);
 app.use(messageRouter);
 
-// // Serve static files
-// app.use(express.static(path.join(__dirname, "./client/build")));
+// Serve static files
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-// // Handle all remaining routes by serving the index.html file
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+// Handle all remaining routes by serving the index.html file
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`.bgGreen);
